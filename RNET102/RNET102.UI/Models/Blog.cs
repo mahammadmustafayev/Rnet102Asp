d2027a54ac77b98fed1b148ce61be8337b93c885
Mahammad V. Mustafayev <mahammadvm@code.local>
2023-06-08 13:29:37 +0400

    added rnet102 web

---
+++ RNET102/RNET102.UI/Models/Blog.cs
@@ -0,0 +1,55 @@
+﻿namespace RNET102.UI.Models;
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string ImageURL { get; set; } = null!;
+    public DateTime PublishDate { get; set; }
+    public string Title { get; set; } = null!;
+    public string Content { get; set; } = null!;
+
+}
+
+
+public static class SeedData
+{
+    public static List<Blog> BlogsSeedData = new()
+    {
+        new Blog{Title="Sed ut perspiciatis unde omnis iste",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+        new Blog{Title="Donec sagittis euismod purus",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+        new Blog{Title="Natus error sit voluptatem",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+        new Blog{Title="Free Shopping",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+        new Blog{Title="Eget elementum vel",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+        new Blog{Title="Mauris accumsan nulla",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+        new Blog{Title="Praesent justo dolor",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+        new Blog{Title="Eget elementum vel",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+        new Blog{Title="Donec sagittis euismod purus",
+            Content="Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum.",
+            ImageURL="https://media.istockphoto.com/id/1140180560/photo/colored-powder-explosion-on-black-background.jpg?s=612x612&w=0&k=20&c=gH_tj4MVWr0226qcq_uJ4nxfLTKG-7T29rtUklT4Obk=",
+            PublishDate=DateTime.Now.AddDays(-1)},
+    };
+}
---
+++ RNET102/RNET102.UI/ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RNET102.UI.ViewComponents;
+
+
+public class BlogViewComponent : ViewComponent
+{
+	public async Task<IViewComponentResult> InvokeAsync()
+	{
+		var blogs = Models.SeedData.BlogsSeedData;
+		return View(model:blogs);
+	}
+}
